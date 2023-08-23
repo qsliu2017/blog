d9ee03ea9455794b0200496b4181c3f8e26166e0
qsliu <qsliu2017@outlook.com>
2023-08-23 22:48:39 +0800

    use tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	experimental: {
-		assets: true,
-	},
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+  experimental: {
+    assets: true
+  }
 });
