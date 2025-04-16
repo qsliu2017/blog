60310e3bbccac53d40fb03a7061e385360ac4d55
qsliu <qsliu2017@outlook.com>
2025-04-16 13:05:24 +0800

    Object.groupBy

--- src/components/note.ts
+++ src/components/note.ts
@@ -4,13 +4,4 @@ export type NoteEntry = CollectionEntry<'notes'>;
 
 export const normalizeId = (id: string) => id.toLowerCase().replace(' ', '-').replaceAll('_', '-');
 
-export const getNotes = async () =>
-	(await getCollection('notes'))
-		.map(note => ({ page: note.data.page, note }))
-		.reduce(
-			(group, { page, note }) => ({
-				...group,
-				[page]: [note, ...(group[page] || [])],
-			}),
-			{} as { [K in NoteEntry['data']['page']]: NoteEntry[] },
-		);
+export const getNotes = async () => Object.groupBy(await getCollection('notes'), note => note.data.page);
